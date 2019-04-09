a24cad45d95fac1a8bb3685b4dc549a195531364
cota <jurbinafernandez@hotmail.com>
2019-04-08 19:41:55 -0500

    Instancias y campos vacios

--- app/src/main/java/com/caotic/juf/demogit/MainActivity.java
+++ app/src/main/java/com/caotic/juf/demogit/MainActivity.java
@@ -4,19 +4,25 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private Button btn;
+    private TextView tv_u,tv_c;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         btn = findViewById(R.id.btn);
+        tv_c = findViewById(R.id.tv_c);
+        tv_u = findViewById(R.id.tv_u);
         btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Toast.makeText(MainActivity.this, "Hola", Toast.LENGTH_SHORT).show();
+                if(tv_u.getText().equals("")|tv_c.getText().equals("")){
+                    Toast.makeText(MainActivity.this, "Campos vacios", Toast.LENGTH_SHORT).show();
+                }
             }
         });
     }
